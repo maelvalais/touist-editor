dbe127aafa729a64bb3b89c858fc9a6de75a1d32
Maël Valais <mael.valais@gmail.com>
2018-06-15 21:15:58 +0200

    added env variable API_URL so that we can change it easily

--- touist-ide/src/config.js
+++ touist-ide/src/config.js
@@ -1,8 +1,10 @@
 const VERSION = '0.1';
 const isDev = process.env.NODE_ENV !== 'production';
+const api_url = process.env.API_URL || "/";
 
 export default {
   isDev,
   version: `v${VERSION}${isDev ? '-dev' : ''}`,
   solvers: ['sat', 'qbf'],
+  api_url: api_url.replace(/\/$/, ""),
 };
